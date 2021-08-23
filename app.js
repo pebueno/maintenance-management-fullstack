2890bcee4159d32eba0230f3cc2705f324532277
pebueno <pedroivobu@gmail.com>
2021-08-22 22:46:25 -0300

    Fixing Deploy to Heroku

--- app.js
+++ app.js
@@ -21,19 +21,16 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) => res.send("Hello world!"));
+app.use(express.static("client/build"));
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+});
 
 // Use Routes
 app.use("/api/companies", companies);
 app.use("/api/assets", assets);
 app.use("/api/units", units);
 app.use("/api/users", users);
-
-// Accessing the path module
-app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.get("*", function (request, response) {
-  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
 
 const port = process.env.PORT || 8082;
 app.listen(port, () => console.log(`Server running on port ${port}`));
